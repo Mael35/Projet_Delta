0e20cee3019a3f447f4026dcf1d2264593e76474
Mael Police <police@et.esiea.fr>
2022-03-14 18:03:41 +0100

    Feat : creation du header et du footer

---
+++ front/reactjs_front_api/src/components/Header.js
@@ -0,0 +1,10 @@
+import logo from './logo.svg';
+
+export default function Header() {
+    return (
+        <header className="App-header">
+            <h1 className="App-title">ESIEA - DELTA - BLOG</h1>
+            <img src={logo} className="App-logo" alt="logo" />
+        </header>
+    )
+}
