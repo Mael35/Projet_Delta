a8b39bf5f46b4a1b26e81bb8c0fd52b1d67e0382
Mael Police <police@et.esiea.fr>
2022-03-16 00:03:48 +0100

    Feat : creation de categories

--- front/reactjs_front_api/src/App.js
+++ front/reactjs_front_api/src/App.js
@@ -1,14 +1,148 @@
 import './App.css';
 
+import { useEffect, useState } from 'react';
+
+import TextField from '@material-ui/core/TextField';
+import AutocompleteCategory from '@material-ui/lab/Autocomplete';
+
 import Header from './components/Header';
 import Footer from './components/Footer';
 
+import NewCategory from './components/newCategory/NewCategory';
+import './components/newCategory/newcategory.css';
+
 
 function App() {
+
+  const [allCategories, setAllCategories] = useState([
+    {
+      "id": 1,
+      "categoryName": "categoryName1",
+    },
+
+    {
+      "id": 2,
+      "categoryName": "categoryName2",
+    }
+  ]);
+  
+  const [newCategory, setNewCategory] = useState({
+    categoryName: "",
+  });
+
+  const [postingCategory, setPostingCategory] = useState(false);
+  const [inputInvalid, setInputInvalid] = useState(false);
+
+  const [myOptions, setMyOptions] = useState([])
+  
+  const getDataFromAPI = () => {
+    console.log("Options Fetched from API")
+  
+    fetch('http://localhost:8080/api/private/category').then((response) => {
+      return response.json()
+    }).then((res) => {
+      console.log(res.data)
+      for (var i = 0; i < res.data.length; i++) {
+        myOptions.push(res.data[i].categoryName)
+      }
+      setMyOptions(myOptions)
+    })
+  }
+  
+  useEffect(() => {
+    fetch('http://localhost:8080/api/private/category')
+    .then(res => res.json())
+    .then(data => {
+      setAllCategories(data);
+    })
+    .catch(e => console.log(e.toString()));
+  }, [postingCategory]);
+  
+  function initInvalidInput() {
+    setInputInvalid(false);
+  }
+
+  function isString( value){
+    return value.match(/^.*[<>/\\].*$/);
+  }
+
+  function submitCategory() {
+    setPostingCategory(true);
+  }
+
+  function newCategoryChange(event) {
+    const {name, value} = event.target;
+    isString(value) ?
+        setInputInvalid("Vous ne pouvez pas inserer de caracteres speciaux") 
+        :
+        value.length > 255 ?
+            setInputInvalid("Le nombre maximum est de 255 caractere")
+            :
+            setNewCategory(prevState => {
+            initInvalidInput();
+
+            return {...prevState,
+                id: allCategories.length + 1,
+                [name]: value
+            }
+            });
+  }
+
+  useEffect(() =>{
+    if (postingCategory) {
+      
+      fetch('http://localhost:8080/api/private/category', {
+          method: "POST",
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(newCategory)
+      })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        setPostingCategory(false);
+        setNewCategory(prevState => {
+          initInvalidInput();
+
+          return {...prevState,
+              id: -1,
+              categoryName: ""
+          }
+          });
+      })
+      .catch(e => console.log(e.toString()));
+    }
+  }, [postingCategory]);
+  
   return (
     <div className="App">
       
       <Header />
+
+      <main>
+        <AutocompleteCategory
+            freeSolo
+            autoComplete
+            autoHighlight
+            options={myOptions}
+            renderInput={(params) => (
+              <TextField {...params}
+                onChange={getDataFromAPI}
+                variant="outlined"
+                label="Categories"
+              />
+            )}
+        />
+        
+        <NewCategory 
+          newCategory={newCategory}
+          newCategoryChange={newCategoryChange}
+          submitCategory={submitCategory}
+          inputInvalid={inputInvalid} 
+        />
+      </main>
 
       <Footer />
 
---
+++ front/reactjs_front_api/src/components/newCategory/NewCategory.js
@@ -0,0 +1,24 @@
+export default function NewProduct(props) {
+
+    // the inputs are controled by the state in App.js
+
+    return (
+        <div className="newCategory__form">
+            <h3 className="newCategory__title" >Ajouter une categorie</h3>
+            <div className="newCategory__container">
+                <label>Nom de la categorie : {" "} </label>
+                <input
+                    className="newCategory__input" 
+                    type="texte"
+                    placeholder="Choisir un nom" 
+                    name="nom"
+                    value={props.newCategory.name}
+                    onChange={props.handleChange} 
+                />
+            </div>
+            {props.inputInvalid && <p>{props.inputInvalid}</p>}
+
+            <button className="newCategory__submitButton" onClick={props.submitCategory}>Nouvelle Categorie</button>
+        </div>
+    )
+}
